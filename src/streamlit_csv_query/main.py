198262bf45c19c7c8ef561037fe84bb80d00d2bb
David Harting <hartingdavid@icloud.com>
2024-07-09 14:40:39 -0400

    Handle CSV files
    still need to update the test for this

--- src/streamlit_csv_query/main.py
+++ src/streamlit_csv_query/main.py
@@ -1,9 +1,45 @@
 import streamlit as st
+import tempfile
+import duckdb
+from pathlib import Path
+from typing import List
+
+
+def path_from_root(*args: List[str]) -> Path:
+    root_path = Path.cwd()
+    return root_path.joinpath(*args)
 
 
 def main():
-    user_input = st.text_input("Enter some text", key="text")
-    st.text(f"You entered: {user_input}")
+    st.title("Query a CSV file")
+
+    st.header("Pick your file")
+    csv_selection = st.selectbox(
+        label="",
+        options=["USA City Populations (2024)", "Upload my own"],
+    )
+
+    relation = None
+
+    if csv_selection == "USA City Populations (2024)":
+        relation = duckdb.from_csv_auto(
+            path_from_root("data", "2024_usa_population.csv")
+        )
+    elif csv_selection == "Upload my own":
+        file_input = st.file_uploader("Upload a CSV file", type="csv")
+
+        if file_input is not None:
+            relation = duckdb.from_csv_auto(file_input)
+
+    if relation is not None:
+        st.header("Structure of the CSV file")
+        table = {
+            "Column Name": relation.columns,
+            "Data Type": relation.dtypes,
+        }
+
+        # Display the dataframe with Streamlit
+        st.table(table)
 
 
 if __name__ == "__main__":
